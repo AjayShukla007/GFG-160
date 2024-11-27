f450ab64992040e3197941c31e118641829a1b70
AjayShukla007 <shuklaajay0838@gmail.com>
2024-11-27 14:00:00 +0530

    feat: Add Go implementations of array algorithms - Port JavaScript solutions to Go - Add array rotation and next permutation algorithms

--- go/main.go
+++ go/main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"fmt"
+	"slices"
 )
 
-func secondMax(arr []int) int {
+func secondMax(arr []int)int  {
 	if len(arr) < 2 {
 		return arr[0]
 	}
@@ -22,7 +23,6 @@ func secondMax(arr []int) int {
 
 	return secMax
 }
-
 // moveZeroToEnd moves all non-zero elements to the front of the array while maintaining their relative order
 // and pushes all zeros to the end. It works by:
 // 1. Using a 'count' variable to keep track of where the next non-zero element should be placed
@@ -38,21 +38,68 @@ func moveZeroToEnd(arr []int) []int {
 	}
 	return arr
 }
-func reverseArr(arr []int) []int {
+func reverseArr(arr []int) []int  {
 	if len(arr) < 2 {
 		return arr
 	}
-	end := len(arr) - 1
+	end := len(arr)-1;
 	for i := 0; i < end; i++ {
 		arr[i], arr[end] = arr[end], arr[i]
 		end--
 	}
 	return arr
 }
-func main() {
+func reverseArrTod(arr []int, start int, end int)  {
+	for i := start; i < end; i++ {
+		arr[i], arr[end] = arr[end], arr[i]
+		end -= 1
+	}
+}
+func rotateArr(arr []int, d int)[]int  {
+	n := len(arr)
+	d %= n
+
+	reverseArrTod(arr, 0, d-1)
+	reverseArrTod(arr, d, n-1)
+	reverseArrTod(arr, 0, n-1)
+	return arr
+}
+func nextPermutation(arr []int)[]int  {
+	n := len(arr)
+	p := -1
+
+	for i := n-2; i >= 0; i-- {
+		if arr[i] < arr[i+1] {
+			p = i
+			break
+		}
+	}
+	if p == -1 {
+		slices.Reverse(arr)
+		return arr
+	}
+
+	for i := n-1; i > p; i-- {
+		if arr[i] > arr[p] {
+			arr[i], arr[p] = arr[p], arr[i]
+			break
+		}
+	}
+	end := n-1
+	for start := p+1; start < end; start++ {
+		arr[start], arr[end] = arr[end], arr[start]
+		end--
+	}
+	return arr
+}
+func main()  {
 	// fmt.Println(secondMax([]int{12, 35, 1, 10, 34, 1}))
 	// arr := []int{1, 2, 0, 4, 3, 0, 5, 0}
 	// fmt.Println(moveZeroToEnd(arr))
-	arr2 := []int{1, 4, 3, 2, 6, 5}
-	fmt.Println(reverseArr(arr2))
+	// arr2 := []int{1, 4, 3, 2, 6, 5}
+	// fmt.Println(reverseArr(arr2))
+	// fmt.Println(rotateArr([]int{1, 2, 3, 4, 5}, 2))
+	// fmt.Println(rotateArr([]int{1,2,3,4,5,6,7}, 3))
+	fmt.Println(nextPermutation([]int{2, 4, 1, 7, 5, 0}));
+	fmt.Println(nextPermutation([]int{1, 2, 3, 6, 5, 4}));
 }
